70ffaf5e7e291cf370538499673dac93490c76f0
Jeremie Melly <melly.jeremie@gmail.com>
2023-03-28 14:03:26 +0200

    Fix: Quizz page state, pending...

--- pages/[locale]/auth/quiz.js
+++ pages/[locale]/auth/quiz.js
@@ -1,116 +1,118 @@
-import Link from '../../../components/Link';
 import { useTranslation } from 'next-i18next';
 import { getStaticPaths, getI18nProps } from '../../../lib/getStatic';
 import { Header } from '../../../components/Header';
 import { Footer } from '../../../components/Footer';
 import { useState, useEffect } from "react";
 import { userService } from '../../../services/user.service';
-import { useRouter } from 'next/router';
-import { use } from '../../../node_modules/i18next/index';
 
 const Quiz = () => {
     const { t } = useTranslation(['common']);
-    const [memberData, setMemberData] = useState(null);
-    const [question, setQuestion] = useState(null);
-    const [answer, setAnswer] = useState(null);
-    const [userQuiz, setUserQuiz] = useState(null);
-    const [userAnswer, setUserAnswer] = useState(null);
     const [isLoading, setIsLoading] = useState(false);
-    const [isUserAnswerLoading, setIsUserAnswerLoading] = useState(false)
-    const [data, setData] = userState(null);
-
-
+    const [data, setData] = useState({
+        memberData: null,
+        question: null,
+        answer: null,
+        userQuiz: null,
+        userAnswer: null,
+    });
 
     useEffect(() => {
         const fetchData = async () => {
-            setIsLoading(true);
-            const currentQuestion = await userService.getCurrentQuestion(memberData._id);
-            await updateComponentData(currentQuestion);
+            setIsLoading(true)
+            const currentQuestion = await userService.getCurrentQuestion(data.memberData._id);
+            setData(prevData => ({
+                ...prevData,
+                question: currentQuestion,
+                answer: currentQuestion.correctOption ? currentQuestion.correctOption : null,
+            }))
+            // await fetchUserAnswer();
+            // await updateComponentData(currentQuestion);
             setIsLoading(false);
         };
-
-        if(memberData)
+        if(data.memberData)
             fetchData();
-  
-    }, [memberData]);
+    }, [data.memberData]);
 
     useEffect(() => {
-        if(!memberData) {
-            setMemberData(userService.memberValue)
+        if(!data.memberData) {
+            setData(prevData => (
+                {
+                    ...prevData,
+                    memberData: userService.memberValue
+                }
+            ))
         }
-    }, []);
+    });
 
-    useEffect(() => {
-
-    }, [answer]);
-
-
-    // New useEffect for handling userAnswer loading
+    // OK
     const fetchUserAnswer = async () => {
-        setIsUserAnswerLoading(true);
-        const userQuizData = await userService.getUserQuiz(memberData._id);
-        setUserQuiz(userQuizData);
-        const currentQuestionAnswer = userQuizData.userAnswers.find(answer => answer.question_id === question.id);
-        setUserAnswer(currentQuestionAnswer ? currentQuestionAnswer.answer : null);
-        setIsUserAnswerLoading(false);
+        const userQuizData = await userService.getUserQuiz(data.memberData._id);
+        const currentQuestionAnswer = userQuizData.userAnswers.find(answer => answer.question_id === data.question.id);
+        setData(prevData => ({
+            ...prevData,
+            userQuiz: userQuizData,
+            userAnswer: currentQuestionAnswer ? currentQuestionAnswer.answer : null
+        }))
     };
 
     useEffect(() => {
-        setAnswer(null);
-        setUserAnswer(null);
-        if (question) {
+        if (data.question) {
             fetchUserAnswer();
         }
-    }, [question]);
+    }, [data.question]);
 
 
-
+    // OK
     const getNextQuestion = async () => {
-        console.log("next question");
         setIsLoading(true);
-        const nextQuestion = await userService.getNextQuestion(memberData._id);
-        await updateComponentData(nextQuestion, true);
+        const nextQuestion = await userService.getNextQuestion(data.memberData._id);
+        setData(prevData => ({
+            ...prevData,
+            question: nextQuestion,
+            answer: nextQuestion.correctOption ? nextQuestion.correctOption : null,
+            userAnswer: null
+        }))
+        await fetchUserAnswer();
         setIsLoading(false);
-        fetchUserAnswer();
     };
 
 
     const submitAnswer = async (selectedAnswer) => {
         setIsLoading(true);
-        const response = await userService.postAnswer(memberData._id, question.id, selectedAnswer)
-        await updateComponentData(question, false, response)
+        const response = await userService.postAnswer(data.memberData._id, data.question.id, selectedAnswer)
+        console.log(response)
+        setData(prevData => ({
+            ...prevData,
+            userAnswer: selectedAnswer,
+            answer: response.correct_answer
+        }))
+        // await updateComponentData(data.question, false, response)
         setIsLoading(false);
     };
 
-    const updateComponentData = (questionData, resetUserAnswer = false, newAnswer = null) => {
-        return new Promise(async (resolve) => {
-            setQuestion(questionData)
-            questionData.correctOption && setAnswer(questionData.correctOption)
-            
-            const userQuizData = await userService.getUserQuiz(memberData._id);
-            setUserQuiz(userQuizData);
-            const currentQuestionAnswer = question ? userQuizData.userAnswers.find(answer => answer.question_id === question.id) : null;
-            if (currentQuestionAnswer && !resetUserAnswer) {
-                setUserAnswer(currentQuestionAnswer.answer);
-            } else {
-                setUserAnswer(null);
-            }
-
-            if (newAnswer) {
-                
-                setAnswer(newAnswer.correct_answer);
-            }
-            resolve();
-
-        });
-    };
-
-
+    // const updateComponentData = async (questionData, resetUserAnswer = false, newAnswer = null) => {
+    //     console.log("Update component data")
+    //     let tmpData = {...data, question: questionData};
+    //     questionData.correctOption && (tmpData = {...tmpData, answer: questionData.correctOption}); // Tocheck in submitAnswer
+        
+    //     const userQuizData = await userService.getUserQuiz(data.memberData._id);
+    //     tmpData = {...tmpData, userQuiz: userQuizData};
+    //     const currentQuestionAnswer = tmpData.question ? userQuizData.userAnswers.find(answer => answer.question_id === tmpData.question.id) : null;
+    //     if (currentQuestionAnswer && !resetUserAnswer) {
+    //         tmpData = {...tmpData, userAnswer: currentQuestionAnswer.answer};
+    //     } else {
+    //         tmpData = {...tmpData, userAnswer: null}
+    //     }
+    //     if (newAnswer) {
+    //         tmpData = {...tmpData, userAnswer: newAnswer.correct_answer};
+    //     }
+    //     setData(tmpData)
+    // };
 
     return (
         <>
             {
-                !isLoading && !isUserAnswerLoading && memberData ? (
+                !isLoading && data.memberData ? (
                     <>
                         <main>
                             <Header title={t('title')} />
@@ -118,10 +120,10 @@ const Quiz = () => {
                                 <div className="flex justify-between items-center flex-row w-full">
                                     <div className="w-16"></div>
                                     <div className="text-2xl font-bold py-3">Quiz</div>
-                                    <div className="w-16 font-bold" >{userQuiz ? userQuiz.score + " pts" : ""}</div>
+                                    <div className="w-16 font-bold" >{data.userQuiz ? data.userQuiz.score + " pts" : ""}</div>
                                 </div>
 
-                                {question && question.id === 0 && (
+                                {data.question && data.question.id === 0 && (
                                     <>
                                         <div className="flex items-center text-sm justify-left m-2 px-6 py-2 border border-stone-500 bg-stone-200 rounded-lg">
                                             {t('quiz-intro')}
@@ -134,34 +136,34 @@ const Quiz = () => {
                                         </button>
                                     </>
                                 )}
-                                {question && question.id > 0 && (
+                                {data.question && data.question.id > 0 && (
                                     <>
                                         <div className="flex justify-around w-full">
-                                            <div className="text-xl font-bold">Question {userQuiz ? userQuiz.completedQuestions + (userAnswer ? 0 : 1) : ""}</div>
-                                            <div className="border border-stone-500 px-2 py-1 rounded">{t(question.difficulty.toLowerCase())}</div>
+                                            <div className="text-xl font-bold">Question {data.userQuiz ? data.userQuiz.completedQuestions + (data.userAnswer ? 0 : 1) : ""}</div>
+                                            <div className="border border-stone-500 px-2 py-1 rounded">{t(data.question.difficulty.toLowerCase())}</div>
                                         </div>
 
                                         <div className="flex items-center text-sm justify-left m-2 px-6 py-4 border border-stone-500 bg-stone-200 rounded-lg ">
-                                            {t("questions." + question.questionText)}
+                                            {t("questions." + data.question.questionText)}
                                         </div>
                                         <div className="flex flex-col gap-2 my-3 w-3/4">
                                             {['a', 'b', 'c', 'd'].map((option) => (
                                                 <button
                                                     key={option}
                                                     className={`inline-block px-7 py-4 text-black font-medium text-m leading-tight rounded shadow-md bg-stone-100 w-full sm:w-auto mt-4 sm:mt-0
-                                            ${userAnswer ? '' : 'hover:bg-stone-400 hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out'}
-                                            ${option === answer && userAnswer && option !== userAnswer ? 'border-2 border-green-700' : ''}
-                                            ${option !== answer && userAnswer && option === userAnswer ? 'border-2 border-red-700' : ''}
+                                            ${data.userAnswer ? '' : ' hover:bg-stone-400 hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out'}
+                                            ${option === data.answer && data.userAnswer && option !== data.userAnswer ? ' border-2 border-green-700' : ''}
+                                            ${option !== data.answer && data.userAnswer && option === data.userAnswer ? ' border-2 border-red-700' : ''}
                                         `}
                                                     onClick={() => {
-                                                        userAnswer ? null : submitAnswer(option)
+                                                        data.userAnswer ? null : submitAnswer(option)
                                                     }}
                                                 >
-                                                    {t("questions." + question[`option${option.toUpperCase()}`])}
+                                                    {t("questions." + data.question[`option${option.toUpperCase()}`])}
                                                 </button>
                                             ))}
 
-                                            {userAnswer ?
+                                            {data.userAnswer ?
                                                 <button className="text-center inline-block px-6 py-2.5 text-white font-medium text-xs leading-tight uppercase rounded 
                                                                     shadow-md bg-green-900 hover:bg-stone-400 hover:shadow-lg focus:shadow-lg focus:outline-none 
                                                                     focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full my-3"
@@ -174,7 +176,7 @@ const Quiz = () => {
                                         </div>
                                     </>
                                 )}
-                                {question && question.id < 0 && <div className="text-xl font-bold">Quiz is over</div>}
+                                {data.question && data.question.id < 0 && <div className="text-xl font-bold">Quiz is over</div>}
                             </div>
                         </main>
 
